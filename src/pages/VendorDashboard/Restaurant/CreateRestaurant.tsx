4561df638a5cb6b08eb25e60da2a08f003a71ca8
opuopu <nurmdopu428@gmail.com>
2024-12-17 18:55:37 +0600

    Fix

--- src/pages/VendorDashboard/Restaurant/CreateRestaurant.tsx
+++ src/pages/VendorDashboard/Restaurant/CreateRestaurant.tsx
@@ -49,7 +49,7 @@ const CreateRestaurant = () => {
   const libraries: any = ["places"];
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey: "AIzaSyBFi80uuJIWkkLCpodFa8oXmD8XD_h8LMc",
+    googleMapsApiKey: "",
     libraries,
   });
 
